97900d1cd2ddbc065d436e51f9e5278af0f3d901
Sumit sharma <sumit8104608630@gmail.com>
2026-05-03 15:14:31 +0530

    completed the metting arranging part

--- frontend/utils/axiosconfig.js
+++ frontend/utils/axiosconfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance=axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:9090",
+    baseURL: import.meta.env.VITE_BACKEND_URL || "https://focusmeet-ai-web-app.onrender.com",
     withCredentials:true,
     headers:{
         'Content-Type':'application/json'
